73431cff15dbc7345a98788c54baa1ff8fb6f722
킨 <69646118+Roflane@users.noreply.github.com>
2026-01-13 11:16:07 +0400

    Push

---
+++ Products Site/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ASP_NET_03._Razor_Pages_Product_Site.Data;
+using ASP_NET_03._Razor_Pages_Product_Site.Models;
+using Bogus;
+
+namespace ASP_NET_03._Razor_Pages_Product_Site.Services;
+
+public class ProductService(IProductRepository productRepository) {
+    public Task<IEnumerable<Product>> GetProductsAsync()
+        => productRepository.GetProductsAsync();
+
+    public Task<Product> GetProductById(int id)
+        => productRepository.GetProductByIdAsync(id);
+
+    public Product AddProduct(Product product)
+    {
+        var faker = new Faker<Product>()
+            .RuleFor(p => p.Id, f => f.Random.Int(1));
+        product.Id = faker.Generate().Id;
+        if (product.Count > 0) product.IsAvailable = true;
+        productRepository.AddProduct(product);
+        return product;
+    }
+}
